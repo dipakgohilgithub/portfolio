aa83a60e92da4ff0b9fb0465cd7b09778ca9c4f5
“Dipak <dipakgohil72732@gmail.com>
2025-02-24 22:27:05 +0530

    header footer using include js script added.

---
+++ public/js/includeHeaderFooter.js
@@ -0,0 +1,12 @@
+function includeHTML(elementId, filePath) {
+    // Fetch the content from the external file
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            // Insert the fetched HTML content into the specified element
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error("Error including file:", error);
+        });
+}
